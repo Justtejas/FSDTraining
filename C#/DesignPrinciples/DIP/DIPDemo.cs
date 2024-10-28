5218be7f64705f27bdf473747c06c184bfeb783a
Justtejas <tejaspantoji@gmail.com>
2024-10-28 16:21:45 +0530

    DIP and the Assignment on the DIP

---
+++ C#/DesignPrinciples/DIP/DIPDemo.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPrinciples.DIP
+{
+    internal class DIPDemo
+    {
+        public void DIPDemoMethod()
+        {
+            var players = new Dictionary<string, IAudioPlayer>
+            {
+                {"MP3",new MP3Player() },
+                {"WAV",new WAVPlayer() },
+            };
+
+            var musicPlayer = new MusicPlayer(players);
+            musicPlayer.Play("song1.mp3","MP3");
+            musicPlayer.Play("song2.wav","WAV");
+            musicPlayer.Play("song3.mp4","MP4");
+        }
+    }
+}
---
+++ C#/DesignPrinciples/DIP/IAudioPlayer.cs
@@ -0,0 +1,44 @@
+﻿namespace DesignPrinciples.DIP
+{
+    public interface IAudioPlayer
+    {
+        void Play(string file);
+    }
+
+    public class MP3Player : IAudioPlayer
+    {
+        public void Play(string file)
+        {
+            Console.WriteLine("MP3 is playing");
+        }
+    }
+
+    public class WAVPlayer : IAudioPlayer
+    {
+        public void Play(string file)
+        {
+            Console.WriteLine("WAV is playing");
+        }
+    }
+
+    public class MusicPlayer
+    {
+        private readonly Dictionary<string, IAudioPlayer> _players;
+        public MusicPlayer(Dictionary<string, IAudioPlayer> players)
+        {
+            _players = players;
+        }
+
+        public void Play(string file,string format)
+        {
+            if (_players.ContainsKey(format))
+            {
+                _players[format].Play(file);
+            }
+            else
+            {
+                Console.WriteLine("Unsupported File Format");
+            }
+        }
+    }
+}
