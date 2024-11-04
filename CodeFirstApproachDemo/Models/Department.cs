c587bc3a364f1c5dc82efeaee1b442dd70b975ef
Justtejas <tejaspantoji@gmail.com>
2024-11-04 11:07:22 +0530

    cf

---
+++ CodeFirstApproachDemo/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeFirstApproachDemo.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
